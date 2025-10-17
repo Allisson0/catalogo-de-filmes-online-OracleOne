c40c9dbc80ed9a2ea20f743bb7648d6da2a47a40
Allisson0 <allissonhiaggo@gmail.com>
2025-10-16 21:47:06 -0300

    Condicionais

--- src/App.java
+++
@@ -1,5 +0,0 @@
-public class App {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Esse é o Screen Match");
-    }
-}
---
+++ src/Main.java
@@ -0,0 +1,19 @@
+public class Main {
+    public static void main(String[] args){
+        int anoDeLancamento = 2022;
+        boolean incluidoNoPlano = true;
+        double notaDoFilme = 9.2;
+        String tipoPlano = "plus";
+
+        if (anoDeLancamento >= 2022){
+            System.out.println("Lançamento que os clientes estão curtindo.");
+        } else {
+            System.out.println("Filme retrô que vale a pena assistir!");
+        }
+
+        if (incluidoNoPlano || tipoPlano.equals("plus")){
+            
+        }
+
+    }
+}
